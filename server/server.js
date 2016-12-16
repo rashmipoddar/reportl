9b8e50ecddcb0e966d522c776db80901edf94f4d
Sam Phillips <armyf35@gmail.com>
2016-12-16 17:06:53 -0500

    feat(Server): Added .env loading

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config(); // eslint-disable-line global-require
+}
 const app = require('express')();
 
 const port = process.env.PORT || 8000;
