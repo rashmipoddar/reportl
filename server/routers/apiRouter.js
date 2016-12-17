069b867c2ba81a0480d142c916d016df020e7f27
Sam Phillips <armyf35@gmail.com>
2016-12-17 15:59:21 -0500

    feat(apiRouter): Added route for userRouter

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -6,4 +6,9 @@ router.all('*', (req, res, next) => {
   next();
 });
 
+router.use('/users', (req, res, next) => {
+  console.log('apiRouter -> userRouter');
+  next();
+}, userRouter);
+
 module.exports = router;
