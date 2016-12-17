5052cc1b9bea3ddc0e7b44cfa46ab484d1a9b6ed
Sam Phillips <armyf35@gmail.com>
2016-12-17 12:23:12 -0500

    feat(Database): Updated users default migration

--- server/database/migrations/20161216195935_users.js
+++ server/database/migrations/20161216195935_users.js
@@ -1,5 +1,13 @@
 exports.up = knex => knex.schema.createTable('users', (table) => {
   table.increments('id').primary();
+  table.boolean('enabled');
+  table.string('username');
+  table.string('password');
+  table.string('firstname');
+  table.string('lastname');
+  table.string('email');
+  table.timestamps(true, true);
+  table.unique('username');
 });
 
 exports.down = knex => knex.schema.dropTable('users');
