0c2a56fda82ce5243b116e45adb6c3f5fd9934e4
Sam Phillips <armyf35@gmail.com>
2016-12-17 15:54:40 -0500

    feat(Server): Added catch all error route

--- server/server.js
+++ server/server.js
@@ -9,4 +9,8 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
+app.all('*', (req, res) => {
+  res.status(404).send('Wrong place bud');
+});
+
 app.listen(port, () => console.log(`Server listening on *:${port}`));
