4c399769369a8c334711a62e8254028745accb33
Sam Phillips <armyf35@gmail.com>
2016-12-17 12:25:07 -0500

    chore(Database): Moved unique to column instead of declaring later

--- server/database/migrations/20161216195935_users.js
+++ server/database/migrations/20161216195935_users.js
@@ -1,13 +1,12 @@
 exports.up = knex => knex.schema.createTable('users', (table) => {
   table.increments('id').primary();
   table.boolean('enabled');
-  table.string('username');
+  table.string('username').unique();
   table.string('password');
   table.string('firstname');
   table.string('lastname');
   table.string('email');
   table.timestamps(true, true);
-  table.unique('username');
 });
 
 exports.down = knex => knex.schema.dropTable('users');
