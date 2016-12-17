72152dfc4d24ec45b2be4b6adb9f103716d28d3e
Sam Phillips <armyf35@gmail.com>
2016-12-17 15:56:33 -0500

    feat(Server): Added routing for apiRouter

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ require('dotenv').config({
 });
 
 const app = require('express')();
-const db = require('./database/db');
+const apiRouter = require('./routers/apiRouter');
 const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
+app.use('/api', apiRouter);
+
 app.all('*', (req, res) => {
   res.status(404).send('Wrong place bud');
 });
