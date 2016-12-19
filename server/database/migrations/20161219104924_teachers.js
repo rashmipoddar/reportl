ccf17ca56a76813ec06943dd16aacefcbe799adb
Sam Phillips <armyf35@gmail.com>
2016-12-19 17:29:05 -0500

    fix(Migrations): Modified teachers
    Changed user_id to unsigned
    Removed foreign key constraint since departments is made later

--- server/database/migrations/20161219104924_teachers.js
+++ server/database/migrations/20161219104924_teachers.js
@@ -1,10 +1,9 @@
 
 exports.up = knex => knex.schema.createTable('teachers', (table) => {
   table.increments('id').primary();
-  table.integer('user_id');
+  table.integer('user_id').unsigned();
   table.foreign('user_id').references('users.id');
-  table.integer('department_id');
-  table.foreign('department_id').references('departments.id');
+  table.integer('department_id').unsigned();
 });
 
 exports.down = knex => knex.schema.dropTable('teachers');
