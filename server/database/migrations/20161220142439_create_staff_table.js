8afb5f791ee1e1d450a1ab8e2b9bbec79d09683f
Sam Phillips <armyf35@gmail.com>
2016-12-20 14:53:47 -0500

    chore(Database): Renamed staff migration

--- server/database/migrations/20161220142439_create_staff_table.js
+++ server/database/migrations/20161220142439_create_staff_table.js
@@ -2,6 +2,7 @@ exports.up = knex => knex.schema.createTable('staff', (table) => {
   table.increments('id').primary();
   table.integer('user_id').unsigned();
   table.foreign('user_id').references('users.id');
+  table.timestamps(true, true);
 });
 
 exports.down = knex => knex.schema.dropTable('staff');
