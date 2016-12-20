acb375fd3fa7b4dd0edfd7a87e47914bb788bc55
Mattie Lents <mattielents@gmail.com>
2016-12-20 15:07:59 -0500

    Integrated addUser work with revamped frontend

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,20 @@
-export function loginSubmit(login) {
+// import axios from 'axios';
 
+export function loginSubmit(login) {
   return {
     type: 'LOGIN_SUBMITTED',
-    payload: login
+    payload: login,
   };
+}
 
+export function createUser(user) {
+  // TO DO:
+  // post user to server
+  // on creation, return using user data as payload
+  // const request = axios.post('/user', user);
+
+  return {
+    type: 'CREATE_USER',
+    payload: user, // temporary
+  };
 }
--- client/components/app.js
+++ client/components/app.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
 import LoginField from '../containers/login-field';
+import UserForm from '../containers/addUser';
 
 export default class App extends Component {
 
   render() {
-   return (
-     <div>
-       <LoginField />
-     </div>
-   );
+    return (
+      <div>
+        <LoginField />
+        <h1>Create a New User:</h1>
+        <UserForm />
+      </div>
+    );
   }
 }
