56da766478fef09f3e978e4480efcf2b2a2b7494
Rashmi <poddar.rashmi@gmail.com>
2016-12-20 12:32:58 -0500

    set up route for endpoint /teachers

---
+++ server/controllers/teacherController.js
@@ -0,0 +1,29 @@
+const Teacher = require('../models/teacherModel');
+
+const teacherController = {
+  getTeacherById({ params: { id } }, res) {
+    Teacher.forge({ id })
+      .fetch()
+      .then((teacher) => {
+        res.json(teacher);
+      })
+      .catch((err) => {
+        console.log(`teacherController.getTeacherById - Error: ${err}`);
+        res.sendStatus(500);
+      });
+  },
+
+  newTeacher({ body: TeacherData }, res) {
+    Teacher.forge(TeacherData)
+      .save()
+      .then((teacher) => {
+        res.json(teacher);
+      })
+      .catch((err) => {
+        console.log(`teacherController.newTeacher - Error: ${err}`);
+        res.sendStatus(500);
+      });
+  },
+};
+
+module.exports = teacherController;
--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRouter = require('./userRouter');
 const studentRouter = require('./studentRouter');
+const teacherRouter = require('./teacherRouter');
 
 router.all('*', (req, res, next) => {
   console.log('apiRouter');
@@ -11,9 +12,16 @@ router.use('/users', (req, res, next) => {
   console.log('apiRouter -> userRouter');
   next();
 }, userRouter);
+
 router.use('/students', (req, res, next) => {
   console.log('apiRouter -> studentRouter');
   next();
 }, studentRouter);
+
+router.use('/teachers', (req, res, next) => {
+  console.log('apiRouter -> teacherRouter');
+  next();
+}, teacherRouter);
+
 
 module.exports = router;
