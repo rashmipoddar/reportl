d19e07d7a8d061051576d333a45365cad6f53725
Sam Phillips <armyf35@gmail.com>
2016-12-20 10:36:58 -0500

    chore(Schema): Merged teacher migrations, removed old

--- server/database/migrations/20161219170533_teachers.js
+++
@@ -1,7 +0,0 @@
-exports.up = knex => knex.schema.table('teachers', (table) => {
-  table.foreign('department_id').references('departments.id');
-});
-
-exports.down = knex => knex.schema.table('teachers', (table) => {
-  table.dropForeign('department_id');
-});
