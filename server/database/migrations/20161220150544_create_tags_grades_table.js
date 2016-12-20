bd9e0fc82e6076fe1824504f5eced3ab1d00eff9
Sam Phillips <armyf35@gmail.com>
2016-12-20 15:08:26 -0500

    feat(Database): Added tags_grades table

---
+++ server/database/migrations/20161220150544_create_tags_grades_table.js
@@ -0,0 +1,10 @@
+exports.up = knex => knex.schema.createTable('tags_grades', (table) => {
+  table.integer('grade_id').unsigned();
+  table.integer('tag_id').unsigned();
+  table.timestamps(true, true);
+  table.primary(['grade_id', 'tag_id']);
+  table.foreign('grade_id').references('grades.id');
+  table.foreign('tag_id').references('tags.id');
+});
+
+exports.down = knex => knex.schema.dropTable('tags_grades');
