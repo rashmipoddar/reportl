1c76bae1bef78978a054975f5e5e80adaf17a90e
Chris Stair <ccstair@gmail.com>
2016-12-21 11:20:34 -0500

    refactor(Refactored form components to the functional style): Refactor Forms
    Linter was throwing an error about component format so Sam and I refactored the forms.
    
    none

--- client/containers/addUser.js
+++ client/containers/addUser.js
@@ -1,36 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { createUser } from '../actions/index'; // needs to be written
 
-class UserForm extends Component {
-  render() {
-    const { handleSubmit } = this.props;
-    return (
+const UserForm = ({ handleSubmit }) => (
+  <div>
+    <h2>Create User</h2>
+    <form onSubmit={handleSubmit}>
       <div>
-        <h2>Create User</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="firstName">First Name</label>
-            <Field name="firstName" component="input" type="text" />
-          </div>
-          <div>
-            <label htmlFor="lastName">Last Name</label>
-            <Field name="lastName" component="input" type="text" />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <Field name="email" component="input" type="email" />
-          </div>
-          <button type="submit">Submit</button>
-        </form>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" component="input" type="text" />
       </div>
-    );
-  }
-}
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <Field name="email" component="input" type="email" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+);
 
-UserForm = reduxForm({
+UserForm.propTypes = {
+  handleSubmit: React.PropTypes.function,
+};
+
+const UserFormMaker = reduxForm({
   form: 'addUser',
   onSubmit: createUser,
 })(UserForm);
 
-export default UserForm;
+export default UserFormMaker;
--- client/containers/login-field.js
+++ client/containers/login-field.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { loginSubmit } from '../actions/index'; // needs to be written
 
-class LoginField extends Component {
-  render() {
-    const { handleSubmit } = this.props;
-    return (
+const LoginField = ({ handleSubmit }) => (
+  <div>
+    <h2>Login</h2>
+    <form onSubmit={handleSubmit}>
       <div>
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="username">Username</label>
-            <Field name="Class Name" component="input" type="text" />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <Field name="Start Date" component="input" type="password" />
-          </div>
-          <button type="submit">Submit</button>
-        </form>
+        <label htmlFor="username">Username</label>
+        <Field name="Class Name" component="input" type="text" />
       </div>
-    );
-  }
-}
+      <div>
+        <label htmlFor="password">Password</label>
+        <Field name="Start Date" component="input" type="password" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+);
 
-LoginField = reduxForm({
+LoginField.propTypes = {
+  handleSubmit: React.PropTypes.function,
+};
+
+const LoginFieldMaker = reduxForm({
   form: 'login',
   onSubmit: loginSubmit,
 })(LoginField);
 
-export default LoginField;
+export default LoginFieldMaker;
