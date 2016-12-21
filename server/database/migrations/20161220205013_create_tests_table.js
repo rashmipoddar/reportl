8659110d0105fb2be93621b46b56ec8e8aeb423f
Sam Phillips <armyf35@gmail.com>
2016-12-20 21:05:59 -0500

    feat(Database): Add tests table

---
+++ server/database/migrations/20161220205013_create_tests_table.js
@@ -0,0 +1,12 @@
+exports.up = knex => knex.schema.createTable('tests', (table) => {
+  table.increments('id').primary();
+  table.integer('class_id').unsigned();
+  table.string('name');
+  table.text('description');
+  table.integer('duration');
+  table.integer('length');
+  table.timestamps(true, true);
+  table.foreign('class_id').references('classes.id');
+});
+
+exports.down = knex => knex.schema.dropTable('tests');
