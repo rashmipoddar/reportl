5ecf6ab7def7cbfcaa3fa1caf8f983acc6bb57ac
Chris Stair <ccstair@gmail.com>
2016-12-20 19:21:31 -0500

    feat(Added axios post requests to index.js in actions folder so that we can interact with the server
    The payload on each action was changed to an axios post request taking an endpoint and the result of
    
    the form submission as arguments.
    
    Actions now send post requests instead of objects with form data.

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,20 +1,25 @@
+import axios from 'axios';
+
 export function loginSubmit(login) {
+  const request = axios.post('/login', login);
   return {
     type: 'LOGIN_SUBMITTED',
-    payload: login,
+    payload: request,
   };
 }
 
 export function createUser(user) {
+  const request = axios.post('/createuser', user);
   return {
     type: 'CREATE_USER',
-    payload: user,
+    payload: request,
   };
 }
 
 export function makeNewClass(classes) {
+  const request = axios.post('/createclass', classes);
   return {
     type: 'CLASS_CREATED',
-    payload: classes,
+    payload: request,
   };
 }
