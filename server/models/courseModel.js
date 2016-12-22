961e5cee0ac437a71bfc467c3cc2cd3db1045eb7
Rashmi <poddar.rashmi@gmail.com>
2016-12-22 11:23:58 -0500

    Added the models, controllers and routes for classses and courses

---
+++ server/models/courseModel.js
@@ -0,0 +1,13 @@
+const db = require('../database/db');
+
+
+const Course = db.Model.extend({
+  tableName: 'courses',
+  hasTimestamps: true,
+  classes() {
+    return this.hasMany(require('./classModel'));
+  },
+
+});
+
+module.exports = Course;
--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const userRouter = require('./userRouter');
 const studentRouter = require('./studentRouter');
 const teacherRouter = require('./teacherRouter');
-
+const classRouter = require('./classRouter');
+const courseRouter = require('./courseRouter');
 
 router.all('*', (req, res, next) => {
   console.log('apiRouter');
@@ -23,5 +24,15 @@ router.use('/teachers', (req, res, next) => {
   console.log('apiRouter -> teacherRouter');
   next();
 }, teacherRouter);
+
+router.use('/classes', (req, res, next) => {
+  console.log('apiRouter -> classRouter');
+  next();
+}, classRouter);
+
+router.use('/courses', (req, res, next) => {
+  console.log('apiRouter -> courseRouter');
+  next();
+}, courseRouter);
 
 module.exports = router;
