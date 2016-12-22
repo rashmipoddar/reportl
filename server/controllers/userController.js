7f9f90b86e0be4665fa2b3b7560555fd8631f67f
Sam Phillips <armyf35@gmail.com>
2016-12-21 19:53:51 -0500

    feat(Users): Add error message response

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,6 @@
 const User = require('../models/userModel');
 
 const userController = {
-  getUserById({ params: { id } }, res) {
   getAll(req, res) {
     User.fetchAll()
     .then(users => res.json(users))
@@ -11,10 +10,26 @@ const userController = {
     });
   },
 
+  getUserById({ params: { id }, baseUrl, originalUrl }, res) {
     User.forge({ id })
       .fetch()
       .then((user) => {
-        res.json(user);
+        if (user) {
+          res.json(user);
+        } else {
+          res.status(404).json({
+            error: {
+              message: 'Not Found',
+            },
+            request: {
+              endpoint: baseUrl,
+              url: originalUrl,
+              parameters: {
+                id,
+              },
+            },
+          });
+        }
       })
       .catch((err) => {
         console.log(`userController.getUserById - Error: ${err}`);
@@ -22,7 +37,7 @@ const userController = {
       });
   },
 
-  newUser({ body: userData }, res) {
+  newUser({ body: userData, baseUrl, originalUrl }, res) {
     User.forge(userData)
       .save()
       .then((user) => {
@@ -30,7 +45,16 @@ const userController = {
       })
       .catch((err) => {
         console.log(`userController.newUser - Error: ${err}`);
-        res.sendStatus(500);
+        res.status(404).json({
+          error: {
+            message: 'Cannot create user',
+          },
+          request: {
+            endpoint: baseUrl,
+            url: originalUrl,
+            content: userData,
+          },
+        });
       });
   },
 };
