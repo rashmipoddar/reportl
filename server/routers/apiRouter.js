490cb0e844065ba010cccd7037aff8691395fda7
Sam Phillips <armyf35@gmail.com>
2016-12-21 19:52:40 -0500

    chore(BackendRouting): Add conditionals to logging

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -3,25 +3,32 @@ const userRouter = require('./userRouter');
 const studentRouter = require('./studentRouter');
 const teacherRouter = require('./teacherRouter');
 
-
-router.all('*', (req, res, next) => {
-  console.log('apiRouter');
-  next();
-});
+if (process.env.NODE_ENV !== 'production') {
+  router.all('*', (req, res, next) => {
+    console.log('apiRouter');
+    next();
+  });
+}
 
 router.use('/users', (req, res, next) => {
-  console.log('apiRouter -> userRouter');
-  next();
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('apiRouter -> userRouter');
+    next();
+  }
 }, userRouter);
 
 router.use('/students', (req, res, next) => {
-  console.log('apiRouter -> studentRouter');
-  next();
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('apiRouter -> studentRouter');
+    next();
+  }
 }, studentRouter);
 
 router.use('/teachers', (req, res, next) => {
-  console.log('apiRouter -> teacherRouter');
-  next();
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('apiRouter -> teacherRouter');
+    next();
+  }
 }, teacherRouter);
 
 module.exports = router;
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
 
-router.all('*', (req, res, next) => {
-  console.log('userRouter');
-  next();
-});
+if (process.env.NODE_ENV !== 'production') {
+  router.all('*', (req, res, next) => {
+    console.log('userRouter');
+    next();
+  });
+}
 
 router.get('/:id', userController.getUserById);
 router.post('/', userController.newUser);
