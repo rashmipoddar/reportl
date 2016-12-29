37c8db0012a0be3033af8935f39c164d77df10ec
Chris Stair <ccstair@gmail.com>
2016-12-29 16:26:19 -0500

    feat(Added endpoints for the gradeable objects functionality and the gradeable objects type function
    Added put request for gradeable objects as well as a getAll and add functionality for
    
    gradeableobjecttypes
    
    none

--- server/routers/gradeableobjectsRouter.js
+++ server/routers/gradeableobjectsRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const gradeableobjectsController = require('../controllers/gradeableobjectsController');
 
 router.get('/', gradeableobjectsController.getAll);
+router.put('/:id', gradeableobjectsController.changeGradeableObject);
 router.post('/', gradeableobjectsController.newGradeableObject);
 
 module.exports = router;
