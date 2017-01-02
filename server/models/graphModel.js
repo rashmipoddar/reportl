3c8e8b85e4c18e032d3cb186f2bf888954fff014
Chris Stair <ccstair@gmail.com>
2017-01-02 12:22:33 -0500

    feat(package.json, graphmodel): Install highcharts and associate graphmodel
    Adding basic functionality to run charts
    
    none

--- server/models/graphModel.js
+++ server/models/graphModel.js
@@ -1,8 +1,31 @@
 const db = require('../database/db');
+require('./departmentModel');
+require('./courseModel');
+require('./classModel');
+require('./gradeableObjectModel');
+require('./gradeableObjectModelType');
 
 const GraphModel = db.Model.extend({
   tableName: 'graph_data',
   hasTimestamps: false,
+  departments() {
+    return this.hasMany('Department', 'department_id');
+  },
+  course() {
+    return this.hasMany('Course', 'course_id');
+  },
+  classes() {
+    return this.hasMany('Class', 'class_id');
+  },
+  gradeableobjects() {
+    return this.hasMany('GradeableObjectModel', 'gradeableobject_id');
+  },
+  gradeableobjecttypes() {
+    return this.hasMany('GradeableObjectType', 'gradeableobjecttype_id');
+  },
+  users() {
+    return this.hasMany('User', 'student_id');
+  },
 });
 
 module.exports = db.model('GraphModel', GraphModel);
