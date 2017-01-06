81811ed861d1d273f002fe9a0fea7d1b8fc19762
Chris Stair <ccstair@gmail.com>
2017-01-05 19:41:56 -0500

    feat(mydailyschedule): tweaked mydailyschedule
    Added styling to dailyschedule component
    
    none

--- client/containers/myDailySchedule.jsx
+++ client/containers/myDailySchedule.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router';
 import { searchCalendar } from '../actions/index';
 
 const calendarStyle = {
   display: 'block',
   borderStyle: 'solid',
   backgroundColor: 'white',
-  height: '500px',
+  height: '400px',
   width: '500px',
   margin: '10px',
   borderWidth: 'thick',
   fontSize: 'x-large',
+  padding: '5px',
 };
 
 const meetingStyle = {
@@ -19,7 +21,7 @@ const meetingStyle = {
   backgroundColor: 'grey',
   height: '100px',
   width: '500px',
-  margin: '50px 0 0 0',
+  margin: '25px 0 0 0',
   fontSize: '25px',
 };
 
@@ -37,18 +39,23 @@ class renderDailySchedule extends Component {
     return (
       <div>{this.props.calendarSearchResult.map(day => (
         <div>
-          <div>{day.weekDay}</div>
+          <div>Classes for {day.weekDay} {day.dayOfMonth}, {day.month}</div>
           <div>{day.meetings.map(meetings => (
-            <button style={meetingStyle}>
-              <div>{meetings.class.name}</div>
-              <div>{meetings.startTime}</div>
-              <div>{meetings.endTime}</div>
-            </button>
+            <div>
+              <Link to="/lesson">
+                <button style={meetingStyle}>
+                  <div>{meetings.class.name}</div>
+                  <div>{meetings.startTime}</div>
+                  <div>{meetings.endTime}</div>
+                </button>
+              </Link>
+            </div>
             ))}</div>
         </div>
       ))}</div>
     );
   }
+
   render() {
     return (
       <div>
@@ -57,6 +64,7 @@ class renderDailySchedule extends Component {
           <div style={calendarStyle}>
             {this.renderScheduleInformation()}
           </div>
+          {this.props.children}
         </div>
       </div>
     );
@@ -67,6 +75,7 @@ class renderDailySchedule extends Component {
 renderDailySchedule.propTypes = {
   searchCalendar: React.PropTypes.func,
   calendarSearchResult: React.PropTypes.arrayOf(React.PropTypes.object),
+  children: React.PropTypes.arrayOf(React.PropTypes.object),
 };
 
 function mapDispatchToProps(dispatch) {
