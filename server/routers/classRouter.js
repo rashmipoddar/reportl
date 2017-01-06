d8351e4d3b3716296f3ac308f63e06c5dc07a58b
Chris Stair <ccstair@gmail.com>
2017-01-06 08:48:36 -0500

    throwaway work

--- server/routers/classRouter.js
+++ server/routers/classRouter.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const classController = require('../controllers/classController');
 
 router.get('/:id', classController.getClassById);
-// router.get('/teacher/:id', classController.getClassByTeacherId);
 router.get('/', classController.getAll);
 router.post('/', classController.newClass);
 router.delete('/:id', classController.deleteClass);
