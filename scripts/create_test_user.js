da0b3be25f10b70a1c65566839850feb5cb5bdfd
Sam Phillips <armyf35@gmail.com>
2017-01-07 17:34:02 -0500

    Moved test user to script and added npm command for it (make:user)

---
+++ scripts/create_test_user.js
@@ -0,0 +1,22 @@
+const User = require('../server/models/userModel');
+
+User.forge({
+  name: 'test',
+})
+.fetch()
+.then((user) => {
+  if (user) {
+    console.log('Deleting test user');
+    return user.destroy();
+  }
+  console.log('No test user');
+  return false;
+})
+.then(() => User.forge({
+  name: 'test',
+  password: 'test',
+  typeId: 1,
+}).save())
+.then(() => {
+  console.log('Created test user');
+});
