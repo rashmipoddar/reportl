ac6407a80da172404e2cd700289bec10e6862a28
Sam Phillips <armyf35@gmail.com>
2017-01-11 15:08:02 -0500

    feature/Initial-setup (#162)
    * Removed auto setup of DB in production
    
    * Updated Readme to include installation instructions

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# reportl
+# Reportl
+##### School Management Software
+---
+## Initial Setup
+#### Requirements
+Tested with the versions below
+- docker (1.12.5 Build 7392c3b)
+- docker-compose (1.9.0 Build 2585387)
+
+#### Installing
+- [ ] Clone the repo `git clone https://github.com/closurecats/reportl.git`
+- [ ] CD into the folder, by default it will be named reportl `cd reportl`
+- [ ] Build and start the docker app using `docker-compose up`
+- [ ] Open a bash prompt in the web container `docker exec -it reportl_web_1 bash`
+- [ ] Run all the database migrations `npm run db:up` (Wait until the MySQL container is fully initialized)
+
+##### Optional
+- [ ] Add seed data to your database `npm run db:seed`
+
+---
--- server/database/db.js
+++ server/database/db.js
@@ -1,42 +1,10 @@
 const knexConfig = require('./knexfile');
 const knex = require('knex')(knexConfig[process.env.NODE_ENV || 'development']);
 const db = require('bookshelf')(knex);
-const exec = require('child_process').exec;
 
 db.plugin('visibility');
 db.plugin('virtuals');
 db.plugin('bookshelf-camelcase');
 db.plugin('registry');
-
-(function migrate(i) {
-  if (i > 0 && process.env.NODE_ENV === 'production') {
-    knex.migrate.latest({
-      directory: './server/database/migrations',
-    })
-    .then(() => console.log('Migration complete'))
-    .then(() => knex.seed.run({
-      directory: './server/database/seeds',
-    }))
-    .then(() => console.log('Seed Complete'))
-    .then(() => new Promise((resolve, reject) => {
-      exec('npm run make:user', (err, stdout, stderr) => {
-        if (err) {
-          console.log(`EXEC err - ${err}`);
-          reject(err);
-        }
-        console.log(`STDOUT: ${stdout}`);
-        console.log(`STDERR: ${stderr}`);
-        resolve(stdout);
-      });
-    }))
-    .then(() => console.log('Database setup complete'))
-    .catch((err) => {
-      console.log(`Migration error - ${err}`);
-      setTimeout(() => {
-        migrate(i - 1);
-      }, 10 * 1000);
-    });
-  }
-}(10));
 
 module.exports = db;
