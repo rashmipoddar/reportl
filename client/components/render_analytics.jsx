c281bb41d3f6b446903098fa694ceccdb19ba7a8
Chris Stair <ccstair@gmail.com>
2017-01-12 17:18:54 -0500

    refactor(Analytics page): Combined analytics into one page
    Moved the table component in to the main analytics page and then added links to the two other
    
    dashboards
    
    none

--- client/components/render_analytics.jsx
+++ client/components/render_analytics.jsx
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router';
+import Reactable from 'reactable';
 import { getChartData } from '../actions/index';
-import { RenderUserTable } from '../containers/render_user_table';
+
+const Table = Reactable.Table;
 
 class RenderAnalytics extends Component {
-
   componentWillMount() {
-    if (!this.props.gradeData) {
-      this.props.getChartData();
-    }
+    this.props.getChartData();
   }
 
   render() {
+    const tableData = [];
+
+    this.props.gradeData.forEach((tableItem) => {
+      tableData.push({
+        Name: tableItem.users.fullName,
+        Grade: tableItem.grade,
+        Module: tableItem.module.name,
+        Assignment: tableItem.gradeableobjects.objectName,
+        Class: tableItem.classes.name,
+        Department: tableItem.departments.name,
+
+      });
+    });
     return (
       <div>
         <div>
@@ -26,7 +38,18 @@ class RenderAnalytics extends Component {
           >
             <Link to="/classgradegraph">Class Analytics</Link></button>
         </div>
-        <RenderUserTable />
+
+        <Table
+          className="table"
+          data={tableData}
+          itemsPerPage={50}
+          pageButtonLimit={10}
+          sortable={[{ column: 'Grade',
+            sort: function sortFunction(a, b) {
+              return a > b ? 1 : -1;
+            } }]}
+
+        />
       </div>
     );
   }
