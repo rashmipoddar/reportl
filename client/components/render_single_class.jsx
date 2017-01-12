e4c8b3bda8d9827858dfb15cf602694312e9b2ca
Chris Stair <ccstair@gmail.com>
2017-01-12 10:19:13 -0500

    updated styling for course catalog

--- client/components/render_single_class.jsx
+++ client/components/render_single_class.jsx
@@ -11,30 +11,36 @@ class RenderSingleClass extends Component {
     return (
       <div>{this.props.selectedClass.map(classInfo => (
         <div>
-          <div>Course Name: {classInfo.course.name}</div>
-          <div>Class ID: {classInfo.id}</div>
-          <div>Full Name: {this.props.teacher}</div>
+          <div className="classSection">
+            <div>Course Name: {classInfo.course.name}</div>
+            <div>Class ID: {classInfo.id}</div>
+            <div>Full Name: {this.props.teacher}</div>
+          </div>
           <h2>Modules</h2>
-          <div>{classInfo.modules.map(module => (
-            <div>
-              <div>{module.moduleName}</div>
-              <div>{module.percentOfClassGrade}</div>
-              <div>{module.startDate}</div>
-              <div>{module.endDate}</div>
-            </div>
+          <div className="classSection">
+            <div>{classInfo.modules.map(module => (
+              <div>
+                <div>{module.moduleName}</div>
+                <div>{module.percentOfClassGrade}</div>
+                <div>{module.startDate}</div>
+                <div>{module.endDate}</div>
+              </div>
             ))}</div>
+          </div>
           <h2>Students</h2>
-          <div>{classInfo.users.map(user => (
-            <div>
-              <div>{user.imgUrl}</div>
-              <div>{user.fullName}</div>
-              <div>{user.description}</div>
-              <div>{user.dateOfBirth}</div>
-              <div>{user.email}</div>
-              <div>{user.phoneNumber}</div>
-              <div>{user.address}</div>
-            </div>
-          ))}</div>
+          <div className="classSection">
+            <div>{classInfo.users.map(user => (
+              <div>
+                <div>{user.imgUrl}</div>
+                <div>{user.fullName}</div>
+                <div>{user.description}</div>
+                <div>{user.dateOfBirth}</div>
+                <div>{user.email}</div>
+                <div>{user.phoneNumber}</div>
+                <div>{user.address}</div>
+              </div>
+            ))}</div>
+          </div>
         </div>
         ))}
       </div>
@@ -45,7 +51,7 @@ class RenderSingleClass extends Component {
     return (
       <div>
         <h2>{this.props.selectedClass.map(title => (
-          <div>{title.name}</div>
+          <div className="pageTitle">{title.name}</div>
           ))}</h2>
         <div>
           {this.renderSingleClass()}
