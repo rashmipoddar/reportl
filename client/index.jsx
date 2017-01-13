0af9fdb3c297e818570ba62652c059e6f8db5a7a
Rashmi <poddar.rashmi@gmail.com>
2017-01-13 10:51:03 -0500

    Modified student view to only show presentation related to a meeting

--- client/index.jsx
+++ client/index.jsx
@@ -25,6 +25,7 @@ import renderDailySchedule from './containers/myDailySchedule';
 import CreateForm from './components/create_all_forms';
 import RenderUserTable from './containers/render_user_table';
 import Directory from './containers/directory';
+import RenderLessonForStudent from './components/render_lesson_student';
 
 const isAuth = () => !!store.getState().user.id;
 
@@ -74,6 +75,7 @@ ReactDOM.render(
         <Route path="/course" component={RenderClassesforCourse} />
         <Route path="/createCourse" component={CourseForm} />
         <Route path="/lesson" component={RenderLesson} />
+        <Route path="/lesson/student" component={RenderLessonForStudent} />
         <Route path="/calendar" component={RenderCalendar} />
         <Route path="/coursecatalog" component={RenderCourseCatalog} />
         <Route path="/coursecatalog/department" component={RenderDepartments} />
