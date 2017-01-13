f202b069cc2268504377fc315647c8bb385391d3
Chris Stair <ccstair@gmail.com>
2017-01-12 20:14:21 -0500

    feat(setup form): add setup form
    Added form for users setup when they first sign in to the application.
    
    none

---
+++ client/containers/setup_school_forms/render_setup_form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SetupFormList from './setup_form_list';
+import SetupFormDetail from './setup_form_detail';
+
+const RenderSetupForm = () => (
+  <div>
+    <SetupFormList />
+    <SetupFormDetail />
+  </div>
+);
+
+export default RenderSetupForm;
